7c4d76239747a5adc345bccf0e7db690bc1c66d0
celian <stcga@makina-corpus.com>
2014-03-21 18:12:46 +0100

    should return null point

--- rando3d.js
+++ rando3d.js
@@ -1,1 +1,18 @@
+var RANDO = {};
+RANDO.Utils = {};
 
+
+RANDO.Utils.interpol = function(A, B, n){
+    var res = {
+        x: 0,
+        y: 0
+    };
+    
+    if(n=0)
+        return res;
+        
+    res.x = (B[0]-A[0])/2;
+    
+    return res;
+    
+}
--- test/test.rando3d.js
+++ test/test.rando3d.js
@@ -2,11 +2,23 @@ var assert = chai.assert;
 
 describe('Rando3D', function() {
     
-    describe('test', function() {
+    describe('Trapeze grid points ', function() {
 
-        it("should return 3.", function(done) {
-            assert.equal(3, 3);
+        it("should return null points.", function(done) {
+            var A = [0,0];
+            var B = [0,0];
+            var n = 0;
+            assert.equal(RANDO.Utils.interpol(A, B, n).x, 0);
+            assert.equal(RANDO.Utils.interpol(A, B, n).y, 0);
             done();
         });
+        
+        /*it("should have x value equal to 1.", function(done) {
+            var A = [0,0];
+            var B = [2,2];
+            var n = 1;
+            assert.equal(RANDO.Utils.interpol(A, B, n).x, 1);
+            done();
+        });*/
     });
 });
