3413d2b6335f1b6943c63e3ac3e241113a41d227
celian <stcga@makina-corpus.com>
2014-03-28 16:01:35 +0100

    refactoring

--- index.js
+++ index.js
@@ -37,82 +37,80 @@ $("#menu .choice").click(function() {
         x : 0,
         y : 0
     };
+    
     if (b_zone){
+        var dem;
         // Getting data of DEM----------
-        // make serve
-        // http://localhost:8000/api/trek/903488/dem.json
-        // http://localhost:8000/api/trek/903488/profile.json
         $.ajax({
-          url:  "json/dem-pne-" + id + ".json",
-          dataType: 'json',
-          async: false,
-          success: function(data) {
-                //var texture =  new BABYLON.Texture("{{ url_for('static', filename='img/texture-8.jpg') }}", scene);
-            console.log("MNT en entrée : ");
-            console.log(data);
+            url:  "json/dem-pne-" + id + ".json",
+            dataType: 'json',
+            async: false,
+            success: function(data) {
+                console.log("MNT en entrée : ");
+                console.log(data);
 
-            var extent = RANDO.Utils.getExtent(data.extent);
-            var vertices = RANDO.Utils.getVerticesFromDEM(data.resolution, data.altitudes, extent);
-            var resolution = data.resolution;
-            var ll_center = RANDO.Utils.toMeters(data.center);
-            var center = {
-                x: ll_center.x,
-                y: data.center.z,
-                z: ll_center.y
-            };
-            
-            var dem = {
-                "extent"    : extent,
-                "vertices"  : vertices,
-                "resolution": resolution,
-                "center"    : center
-            };
-            translateXY.x = -dem.center.x;
-            translateXY.y = -dem.center.z;
-            
-            RANDO.Utils.translateDEM(
-                dem, 
-                translateXY.x, 
-                dem.extent.altitudes.min, 
-                translateXY.y
-            );
-            
-            console.log("MNT en sortie : prêt à être manipulé par BABYLON.js");
-            console.log(dem);
-            //RANDO.Builds.cardinals(dem.extent, scene);
-            
-            // Zone 
-            RANDO.Builds.zone(scene, dem);
-                
-          }
+                var extent = RANDO.Utils.getExtent(data.extent);
+                var ll_center = RANDO.Utils.toMeters(data.center);
+
+                dem = {
+                    "extent"    :   extent,
+                    "vertices"  :   RANDO.Utils.getVerticesFromDEM(
+                                        data.resolution, 
+                                        data.altitudes, 
+                                        extent
+                                    ),
+                    "resolution":   data.resolution,
+                    "center"    :   {
+                                        x: ll_center.x,
+                                        y: data.center.z,
+                                        z: ll_center.y
+                                    }
+                };
+                translateXY.x = -dem.center.x;
+                translateXY.y = -dem.center.z;
+            }
         });//------------------------------------------------------------
+        
+        
+        dem = RANDO.Utils.translateDEM(
+            dem, 
+            translateXY.x, 
+            dem.extent.altitudes.min, 
+            translateXY.y
+        );
+        
+        console.log("MNT en sortie : prêt à être manipulé par BABYLON.js");
+        console.log(dem);
+        //RANDO.Builds.cardinals(dem.extent, scene);
+        
+        // Zone building
+        RANDO.Builds.zone(scene, dem);
     }
-    var troncon_length;
+
+    var vertices;
     if (b_troncon) {
         // Getting data of TRONCON----------
-        //profile-pne-903488
         $.ajax({
             url: "json/profile-pne-" + id + ".json",
             dataType: 'json',
             async: false,
             success: function(data) {
-                var vertices = RANDO.Utils.getVerticesFromProfile(data.profile);
-                
-                vertices = RANDO.Utils.translateRoute(
-                    vertices, 
-                    translateXY.x, 
-                     altitude_offset, 
-                    translateXY.y
-                );
-                troncon_length = vertices.length;
-                RANDO.Builds.route(scene, vertices);
+                vertices = RANDO.Utils.getVerticesFromProfile(data.profile);
             }
         });//------------------------------------------------------------
+        
+        vertices = RANDO.Utils.translateRoute(
+            vertices, 
+            translateXY.x, 
+            altitude_offset, 
+            translateXY.y
+        );
+        RANDO.Builds.route(scene, vertices);
     }   
        
     // Once the scene is loaded, just register a render loop to render it
     engine.runRenderLoop(function () {
-        RANDO.Utils.refreshPanels(troncon_length, scene);
+        RANDO.Utils.refreshPanels(vertices.length, scene);
         scene.render();
     });
 });
