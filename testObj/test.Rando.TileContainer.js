aac050f5eee676c1f3a93964f8694db08cbd65aa
celian <stcga@makina-corpus.com>
2014-04-30 17:02:23 +0200

    tileContainer test

---
+++ testObj/test.Rando.TileContainer.js
@@ -0,0 +1,37 @@
+
+//WIP
+
+//~ describe('Subdivide grid in tiles - "subdivideGrid()"', function() {
+            //~ var tiles = {};
+            //~ var A = { x : -10, y : 20 };
+            //~ var B = { x : 30, y : 30 };
+            //~ var C = { x : 30, y : 0 };
+            //~ var D = { x : 0, y : -10 };
+            //~ var altitudes = [
+                //~ [10, 10, 10, 10],
+                //~ [10, 10, 10, 10],
+                //~ [10, 10, 10, 10],
+                //~ [10, 10, 10, 10],
+                //~ [10, 10, 10, 10],
+                //~ [10, 10, 10, 10]
+            //~ ];
+            //~ var grid_trapeze = RANDO.Utils.createElevationGrid(
+                //~ A, B, C, D, 
+                //~ altitudes
+            //~ );
+            //~ 
+            //~ it("each grid of tiles array should have a number of columns stable ", function(done) {
+                //~ RANDO.Utils.subdivideGrid(tiles, grid_trapeze, 18);
+                //~ console.log(tiles);
+                //~ 
+                //~ for (it in tiles) {
+                    //~ var grid = tiles[it].grid
+                    //~ var res = grid[0].length;
+                    //~ for (row in grid) {
+                        //~ assert.equal(grid[row].length, res);
+                    //~ }
+                //~ }
+//~ 
+                //~ done();
+            //~ });
+        //~ });
