65efee9edf63ecd4dd47a2c7e8d67f1255d9fdae
celian <stcga@makina-corpus.com>
2014-05-06 19:10:36 +0200

    wip demo geotrek 3D

---
+++ demo/index.js
@@ -0,0 +1,14 @@
+
+window.onload = function onload(){
+
+        RANDO.SETTINGS.DEM_URL      = "json/trek/903488/dem.json",
+        RANDO.SETTINGS.PROFILE_URL  = "json/trek/903488/profile.json";
+        RANDO.SETTINGS.TEXTURE_URL  = "img/texture/903488/texture.jpg";
+        RANDO.START_TIME = Date.now();
+        
+        var canvas = document.getElementById('canvas_renderer');
+        var scene = new RANDO.Scene(canvas, true);
+        scene.init();
+
+};
+
