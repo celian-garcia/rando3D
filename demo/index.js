5912478e7fb62b1e46662ccf272297636ede4a23
celian <stcga@makina-corpus.com>
2014-05-07 14:15:30 +0200

    v1 demo link

--- demo/index.js
+++ demo/index.js
@@ -1,9 +1,9 @@
 
 window.onload = function onload(){
 
-        RANDO.SETTINGS.DEM_URL      = "json/trek/903488/dem.json",
-        RANDO.SETTINGS.PROFILE_URL  = "json/trek/903488/profile.json";
-        RANDO.SETTINGS.TEXTURE_URL  = "img/texture/903488/texture.jpg";
+        RANDO.SETTINGS.DEM_URL      = "../json/trek/903488/dem.json",
+        RANDO.SETTINGS.PROFILE_URL  = "../json/trek/903488/profile.json";
+        RANDO.SETTINGS.TEXTURE_URL  = "../img/texture/903488/texture.jpg";
         RANDO.START_TIME = Date.now();
         
         var canvas = document.getElementById('canvas_renderer');
