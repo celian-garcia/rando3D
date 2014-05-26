5f19916bbb6622f7486ef81a20e981bdf9aae9e8
celian <stcga@makina-corpus.com>
2014-05-26 10:52:38 +0200

    added Camera Object

---
+++ Rando/Rando.Camera.js
@@ -0,0 +1,30 @@
+/*******************************************************************************
+ * Rando.Camera.js
+ * 
+ * Camera class : 
+ *  Permites the build of a Camera 
+ * 
+ * @author: Célian GARCIA
+ ******************************************************************************/
+
+RANDO = RANDO || {};
+
+(function () {  "use strict" 
+    
+    /* Constructor */
+    RANDO.Camera = function (scene) {
+        this._scene         = scene;
+        
+        this.init();
+    };
+
+    /* List of Methods */
+    RANDO.Poi.prototype = {
+        init:                           init,
+    };
+
+    function init () {
+    };
+
+    
+})();
