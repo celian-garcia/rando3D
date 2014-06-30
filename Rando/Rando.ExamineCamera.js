edd85e0fa2ef64883417d872f22cfb9cd0f28e42
celian <stcga@makina-corpus.com>
2014-06-30 13:41:41 +0200

    update events listeners of ExamineCamera (ref #99)

--- Rando/Rando.ExamineCamera.js
+++ Rando/Rando.ExamineCamera.js
@@ -113,15 +113,15 @@ var RANDO = RANDO || {};
         return this.speed * ((BABYLON.Tools.GetDeltaTime() / (BABYLON.Tools.GetFps() * 10.0)));
     };
 
-    RANDO.ExamineCamera.prototype.attachControl = function (canvas, noPreventDefault) {
+    RANDO.ExamineCamera.prototype.attachControl = function (element, noPreventDefault) {
         var previousPosition;
         var that = this;
         var pointerId;
 
-        if (this._attachedCanvas) {
+        if (this._attachedElement) {
             return;
         }
-        this._attachedCanvas = canvas;
+        this._attachedElement = element;
 
         var engine = this._scene.getEngine();
 
@@ -291,40 +291,46 @@ var RANDO = RANDO || {};
             };
         }
 
-        canvas.addEventListener(eventPrefix + "down", this._onPointerDown, false);
-        canvas.addEventListener(eventPrefix + "up", this._onPointerUp, false);
-        canvas.addEventListener(eventPrefix + "out", this._onPointerUp, false);
-        canvas.addEventListener(eventPrefix + "move", this._onPointerMove, false);
-        canvas.addEventListener("mousemove", this._onMouseMove, false);
-        canvas.addEventListener("MSPointerDown", this._onGestureStart, false);
-        canvas.addEventListener("MSGestureChange", this._onGesture, false);
-        window.addEventListener("keydown", this._onKeyDown, false);
-        window.addEventListener("keyup", this._onKeyUp, false);
-        window.addEventListener('mousewheel', this._wheel, false);
-        window.addEventListener('DOMMouseScroll', this._wheel, false);
-        window.addEventListener("blur", this._onLostFocus, false);
+        element.addEventListener(eventPrefix + "down", this._onPointerDown, false);
+        element.addEventListener(eventPrefix + "up", this._onPointerUp, false);
+        element.addEventListener(eventPrefix + "out", this._onPointerUp, false);
+        element.addEventListener(eventPrefix + "move", this._onPointerMove, false);
+        element.addEventListener("mousemove", this._onMouseMove, false);
+        element.addEventListener("MSPointerDown", this._onGestureStart, false);
+        element.addEventListener("MSGestureChange", this._onGesture, false);
+        element.addEventListener('mousewheel', this._wheel, false);
+        element.addEventListener('DOMMouseScroll', this._wheel, false);
+
+        BABYLON.Tools.RegisterTopRootEvents([
+            { name: "keydown", handler: this._onKeyDown },
+            { name: "keyup", handler: this._onKeyUp },
+            { name: "blur", handler: this._onLostFocus }
+        ]);
     };
 
-    RANDO.ExamineCamera.prototype.detachControl = function (canvas) {
-        if (this._attachedCanvas != canvas) {
+    RANDO.ExamineCamera.prototype.detachControl = function (element) {
+        if (this._attachedElement != element) {
             return;
         }
 
-        canvas.removeEventListener(eventPrefix + "down", this._onPointerDown);
-        canvas.removeEventListener(eventPrefix + "up", this._onPointerUp);
-        canvas.removeEventListener(eventPrefix + "out", this._onPointerUp);
-        canvas.removeEventListener(eventPrefix + "move", this._onPointerMove);
-        canvas.removeEventListener("mousemove", this._onMouseMove);
-        canvas.removeEventListener("MSPointerDown", this._onGestureStart);
-        canvas.removeEventListener("MSGestureChange", this._onGesture);
-        window.removeEventListener("keydown", this._onKeyDown);
-        window.removeEventListener("keyup", this._onKeyUp);
-        window.removeEventListener('mousewheel', this._wheel);
-        window.removeEventListener('DOMMouseScroll', this._wheel);
-        window.removeEventListener("blur", this._onLostFocus);
+        element.removeEventListener(eventPrefix + "down", this._onPointerDown);
+        element.removeEventListener(eventPrefix + "up", this._onPointerUp);
+        element.removeEventListener(eventPrefix + "out", this._onPointerUp);
+        element.removeEventListener(eventPrefix + "move", this._onPointerMove);
+        element.removeEventListener("mousemove", this._onMouseMove);
+        element.removeEventListener("MSPointerDown", this._onGestureStart);
+        element.removeEventListener("MSGestureChange", this._onGesture);
+        element.removeEventListener('mousewheel', this._wheel);
+        element.removeEventListener('DOMMouseScroll', this._wheel);
+
+        BABYLON.Tools.UnregisterTopRootEvents([
+            { name: "keydown", handler: this._onKeyDown },
+            { name: "keyup", handler: this._onKeyUp },
+            { name: "blur", handler: this._onLostFocus }
+        ]);
 
         this._MSGestureHandler = null;
-        this._attachedCanvas = null;
+        this._attachedElement = null;
 
         if (this._reset) {
             this._reset();
@@ -348,6 +354,7 @@ var RANDO = RANDO || {};
 
         if (this._diffPosition.length() > BABYLON.Engine.CollisionsEpsilon) {
             this.position.addInPlace(this._diffPosition);
+            this.setPosition(this.position);
             if (this.onCollide) {
                 this.onCollide(this._collider.collidedMesh);
             }
