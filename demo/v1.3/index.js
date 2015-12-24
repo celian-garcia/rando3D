c52f4b46eac57e19adcef5d1ee2a7cc5390c30f6
Sbats <simon.bats12@gmail.com>
2015-12-24 14:55:05 +0100

    update v1,3 demo

--- demo/v1.3/index.js
+++ demo/v1.3/index.js
@@ -1,11 +1,28 @@
 
 window.onload = function onload() {
+
+    var callback = null;
+    var container = document.querySelector('#container-3D');
+
+    if (container) {
+        var containerClasses = container.classList;
+        if (!containerClasses.contains('isLoading')) {
+            containerClasses.add('isLoading');
+        }
+
+        callback = function () {
+            if (containerClasses.contains('isLoading')) {
+                containerClasses.remove('isLoading');
+            }
+        }
+    }
+
     var pk = 903280;
     var customSettings = {
-        IMAGES_FOLDER: 'images/',
-        DEM_URL: "http://geotrek-pnc.makina-corpus.net/api/trek/429/dem.json",
-        PROFILE_URL: "http://geotrek-pnc.makina-corpus.net/api/trek/429/profile.json",
-        POI_URL: "http://geotrek-pnc.makina-corpus.net/api/pois/?format=geojson&trek=429",
+        IMAGES_FOLDER: 'img/',
+        DEM_URL: "http://geotrek-pnc.makina-corpus.net/api/fr/treks/429/dem.json",
+        PROFILE_URL: "http://geotrek-pnc.makina-corpus.net/api/fr/treks/429/profile.json",
+        POI_URL: "http://geotrek-pnc.makina-corpus.net/api/fr/treks/429/pois.geojson",
         TILE_TEX_URL: "https://a.tiles.mapbox.com/v3/makina-corpus.i3p1001l/{z}/{x}/{y}.png",
         SIDE_TEX_URL: "img/side.jpg",
         CAM_SPEED_F: 100,
@@ -21,5 +38,5 @@ window.onload = function onload() {
     var cameraID = "examine";
     var app3D = new Rando3D();
     var scene = app3D.init(customSettings, canvas, cameraID);
-    scene.init();
+    scene.init(callback);
 };
