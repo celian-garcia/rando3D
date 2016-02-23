100d5e8af24f2be6bfb2cced99d64baa64f0b0e7
Alexandra Janin <alexandra.janin@makina-corpus.com>
2016-02-23 16:03:57 +0100

    Remove useless files

--- gulp/tasks/default.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-var buildMode = require('../config').buildMode;
-
-if (buildMode.watch) {
-    gulp.task('default', ['watch']);
-} else {
-    gulp.task('default', ['dist']);
-}
